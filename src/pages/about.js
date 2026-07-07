e1b43540c12255559ee59a2119657d773800d5e1
Andrew Moon <ajm80work@gmail.com>
2026-07-07 00:01:11 -0500

    Commit4

--- src/pages/about.js
+++ src/pages/about.js
@@ -8,7 +8,7 @@ export const Head = () => (
     <title>About Page</title>
     <meta
       name="description"
-      content="About Andrew Moon, Front-End Developer, ReactJS, GatsbyJS, Material-UI, Apollo, GraphQL, Python-Django"
+      content="About Andrew Moon, Front-End Developer, ReactJS, Next.js, TypeScript, GatsbyJS, Material-UI, Tailwind CSS, Supabase, Stripe, Apollo, GraphQL, Python-Django"
     />
   </>
 )
@@ -22,8 +22,9 @@ const About = () => {
           Hello, my name is Andrew Moon and I am an aspiring Front-End Developer
           with some knowledge of Back-End Development. My primary focus has been on
           the ReactJS framework. While focusing on ReactJS, I have utilized the
-          following applications in my studies and projects: ReactJS, Python-Django, GatsbyJS, Apollo,
-          GraphQL, JavaScript, HTML, Material-UI, Auth0, and Airtable. Computer programming has
+          following applications in my studies and projects: ReactJS, Next.js, TypeScript,
+          Python-Django, GatsbyJS, Apollo, GraphQL, JavaScript, HTML, Material-UI, Tailwind CSS,
+          Supabase, Stripe, Auth0, Veriff, and Airtable. Computer programming has
           become a great passion of mine in which I enjoy learning and solidifying
           my skills very much. I do not want to become complacent in my programming
           studies. I want to keep track of the latest updates and trends in the
